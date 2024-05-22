8c2775df52e376aa7a42c776c4b24b132f8c0493
Riky Ahmad Fathoni <riky.fathoni@gmail.com>
2024-05-22 11:18:07 +0700

    Implement Hilt for Dependency Injection in App + Configure Hilt Application class + Add Hilt annotations to necessary classes ! Change MainActivity location to ui/main

---
+++ app/src/main/java/com/staygrateful/gamelistapp/App.kt
@@ -0,0 +1,24 @@
+package com.staygrateful.gamelistapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class for TodoListApp.
+ *
+ * This class represents the application class for App.
+ * It initializes Hilt for dependency injection.
+ */
+@HiltAndroidApp
+class App : Application() {
+
+    /**
+     * Called when the application is starting.
+     *
+     * This method is called when the application is starting, before any activity, service, or receiver objects
+     * (excluding content providers) have been created. It initializes any necessary components of the application.
+     */
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/staygrateful/gamelistapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/staygrateful/gamelistapp/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.staygrateful.gamelistapp
+package com.staygrateful.gamelistapp.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalProvider
-import androidx.compose.ui.tooling.preview.Preview
 import com.staygrateful.gamelistapp.ui.theme.GameListAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
