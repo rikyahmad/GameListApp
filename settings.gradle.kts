9d26f8ef40d67815f1a5b31317f944b9047ff777
Riky Ahmad Fathoni <riky.fathoni@gmail.com>
2024-05-22 10:40:36 +0700

    Initial commit: Set up new Android project with Jetpack Compose and Modularization

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GameListApp"
+include(":app")
+include(":core")
+include(":features:feature-list")
+include(":features:feature-search")
+include(":features:feature-detail")
+include(":features:feature-favorites")
